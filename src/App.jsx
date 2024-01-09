0072a67356292b3abe448eed51f01e438d090199
Daniel Oaks <daniel@danieloaks.net>
2024-01-09 19:35:55 +1000

    Setup basic look

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ function App() {
       <ThemeProvider theme={theme}>
         <div className="App">
           <header className="bg-[#4788d5] flex justify-between items-center px-2 py-1">
-            <img src={logo} className="mx-3 my-1.5" alt="DrawRef logo" />
-            <span className="mx-5 my-2 text-white text-lg">Login</span>
+            <a href="/" className="block px-3 py-1.5"><img src={logo} alt="DrawRef logo" /></a>
+            <a href="/login" className="mx-5 my-2 text-white text-lg">Login</a>
           </header>
-          <br/><br/><br/><br/>
-          <Button title="I'm a button from React Native Elements" />
-          <h1 className="text-3xl font-bold">
-            Hello world!
-          </h1>
+          <div id="content" className="text-center text-[#0d2748]">
+            <h1 className="text-3xl font-semibold mt-8 mb-3">
+              Select a category
+            </h1>
+          </div>
           <footer className="bg-[#4788d5] flex gap-1 justify-center items-center px-2 py-1 text-white border-t-[5px] border-t-[#fac2d6]">
             DrawRef &middot; <a href="https://github.com/DanielOaks" className="text-[#d2ebf4]">Source Code</a>
           </footer>
