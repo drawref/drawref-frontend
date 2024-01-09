0ca4caadf4c3ad49c81683358dd87d6536a29f46
Daniel Oaks <daniel@danieloaks.net>
2024-01-09 23:40:43 +1000

    Self-host Open Sans

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import logo from './logo-light.svg';
 import { createTheme, ThemeProvider } from "@rneui/themed";
 import Icon from '@mdi/react';
 import { mdiLoginVariant } from '@mdi/js';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/600.css';
 
 const theme = createTheme({
   lightColors: {
@@ -40,29 +43,29 @@ function App() {
             </div>
           </header>
           <div id="content" className="bg-white text-center text-[#0d2748]">
-            <h1 className="text-3xl font-semibold mt-11 mb-3">
+            <h1 className="text-3xl font-semibold mt-10 mb-3">
               Select a category
             </h1>
-            <div className="flex w-[60rem] max-w-full px-4 mx-auto flex-wrap justify-center gap-5 my-8">
-              <a href="/" className="block w-[15rem] h-[17rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
+            <div className="flex w-[60rem] max-w-full px-4 mx-auto flex-wrap justify-center items-center gap-5 my-8">
+              <a href="/" className="block w-[15rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
-                <h2 className="font-semibold mt-2 text-2xl">Poses</h2>
+                <h2 className="font-medium mt-2 -mb-1 text-2xl">Poses</h2>
               </a>
-              <a href="/" className="block w-[15rem] h-[17rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
+              <a href="/" className="block w-[15rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
-                <h2 className="font-semibold mt-2 text-2xl">Faces</h2>
+                <h2 className="font-medium mt-2 -mb-1 text-2xl">Faces</h2>
               </a>
-              <a href="/" className="block w-[15rem] h-[17rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
+              <a href="/" className="block w-[15rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
-                <h2 className="font-semibold mt-2 text-2xl">Animals</h2>
+                <h2 className="font-medium mt-2 -mb-1 text-2xl">Animals</h2>
               </a>
-              <a href="/" className="block w-[15rem] h-[17rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
+              <a href="/" className="block w-[15rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
-                <h2 className="font-semibold mt-2 text-2xl">Hands</h2>
+                <h2 className="font-medium mt-2 -mb-1 text-2xl">Hands</h2>
               </a>
-              <a href="/" className="block w-[15rem] h-[17rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
+              <a href="/" className="block w-[15rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
-                <h2 className="font-semibold mt-2 text-2xl">Plants</h2>
+                <h2 className="font-medium mt-2 -mb-1 text-2xl">Plants</h2>
               </a>
             </div>
           </div>
