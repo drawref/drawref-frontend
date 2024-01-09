7c1a099eb9966fc8c5b778ce853477ae45cefe4d
Daniel Oaks <daniel@danieloaks.net>
2024-01-09 20:09:14 +1000

    Make header and footer sticky

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <div className="App">
-          <header className="bg-[#4788d5] flex justify-between items-center px-2 py-1">
+          <header className="sticky top-0 bg-[#4788d5] flex justify-between items-center px-2 py-1">
             <a href="/login" className="px-2 py-2 text-white invisible sm:hidden">
               <Icon path={mdiLoginVariant}
                 title="Login"
@@ -43,7 +43,7 @@ function App() {
             <h1 className="text-3xl font-semibold mt-11 mb-3">
               Select a category
             </h1>
-            <div className="flex w-[60rem] mx-auto flex-wrap justify-center gap-5 my-8">
+            <div className="flex w-[60rem] max-w-full px-4 mx-auto flex-wrap justify-center gap-5 my-8">
               <a href="/" className="block w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-[#e1e8ee] border-[5px] flex-shrink-0">
                 <div className="w-full h-[13.5rem] bg-slate-300 rounded"></div>
                 <h2 className="font-semibold mt-2 text-2xl">Poses</h2>
@@ -66,7 +66,7 @@ function App() {
               </a>
             </div>
           </div>
-          <footer className="bg-[#4788d5] flex gap-1 justify-center items-center px-2 pt-1 pb-1.5 text-white border-t-[5px] border-t-[#fac2d6]">
+          <footer className="sticky bottom-0 bg-[#4788d5] flex gap-1 justify-center items-center px-2 pt-1 pb-1.5 text-white border-t-[5px] border-t-[#fac2d6]">
             DrawRef &middot; <a href="https://github.com/DanielOaks" className="text-[#d2ebf4]">Source Code</a>
           </footer>
         </div>
