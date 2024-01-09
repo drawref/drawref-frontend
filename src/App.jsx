fc2290e5dee5015b4a7423410bdcd4079419d623
Daniel Oaks <daniel@danieloaks.net>
2024-01-09 21:13:54 +1000

    Replace custom colours with theme

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <div className="App">
-          <header className="sticky top-0 bg-[#4788d5] flex justify-between items-center px-2 py-1">
+          <header className="sticky top-0 bg-primary-600 flex justify-between items-center px-2 py-1">
             <a href="/login" className="px-2 py-2 text-white invisible sm:hidden">
               <Icon path={mdiLoginVariant}
                 title="Login"
@@ -44,23 +44,23 @@ function App() {
               Select a category
             </h1>
             <div className="flex w-[60rem] max-w-full px-4 mx-auto flex-wrap justify-center gap-5 my-8">
-              <a href="/" className="block w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-[#e1e8ee] border-[5px] flex-shrink-0">
+              <a href="/" className="block w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[13.5rem] bg-slate-300 rounded"></div>
                 <h2 className="font-semibold mt-2 text-2xl">Poses</h2>
               </a>
-              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-[#e1e8ee] border-[5px] flex-shrink-0">
+              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[13.5rem] bg-slate-300 rounded"></div>
                 <h2 className="font-semibold mt-2 text-2xl">Faces</h2>
               </a>
-              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-[#e1e8ee] border-[5px] flex-shrink-0">
+              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[13.5rem] bg-slate-300 rounded"></div>
                 <h2 className="font-semibold mt-2 text-2xl">Animals</h2>
               </a>
-              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-[#e1e8ee] border-[5px] flex-shrink-0">
+              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[13.5rem] bg-slate-300 rounded"></div>
                 <h2 className="font-semibold mt-2 text-2xl">Hands</h2>
               </a>
-              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-[#e1e8ee] border-[5px] flex-shrink-0">
+              <a href="/" className="w-[16rem] h-[18rem] px-3 py-3 shadow-lg border-slate-200 border-[5px] flex-shrink-0">
                 <div className="w-full h-[13.5rem] bg-slate-300 rounded"></div>
                 <h2 className="font-semibold mt-2 text-2xl">Plants</h2>
               </a>
@@ -68,7 +68,7 @@ function App() {
           </div>
           <div className="flex flex-col sticky bottom-0">
             <div className="h-20 bg-gradient-to-b from-transparent to-white sm:hidden"></div>
-            <footer className="bg-[#4788d5] flex gap-1 justify-center items-center px-2 pt-1 pb-1.5 text-white border-t-[5px] border-t-[#fac2d6]">
+            <footer className="bg-primary-600 flex gap-1 justify-center items-center px-2 pt-1 pb-1.5 text-white border-t-[5px] border-t-secondary-200">
               DrawRef &middot; <a href="https://github.com/DanielOaks" className="text-[#d2ebf4]">Source Code</a>
             </footer>
           </div>
