c5c4e2a315e9f10b215fcf353b991ea26b391fee
Daniel Oaks <daniel@danieloaks.net>
2024-01-09 18:45:25 +1000

    Add ReactNativeElements

---
+++ config-overrides.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+const {
+  override,
+  addBabelPlugins,
+  babelInclude,
+  addWebpackAlias,
+  addBabelPresets,
+  removeModuleScopePlugin,
+} = require("customize-cra");
+
+module.exports = override(
+  ...addBabelPlugins([
+    "@babel/plugin-proposal-class-properties",
+    {
+      loose: true,
+    },
+  ]),
+  ...addBabelPresets([
+    "@babel/preset-env",
+    {
+      loose: true,
+      targets: {
+        browsers: ["> 1%", "last 2 versions"],
+      },
+      modules: "commonjs",
+    },
+  ]),
+  removeModuleScopePlugin(),
+  babelInclude(
+    [
+      path.resolve(__dirname, "node_modules/@rneui/base"),
+      path.resolve(__dirname, "node_modules/@rneui/themed"),
+      path.resolve(__dirname, "node_modules/react-native-vector-icons"),
+      path.resolve(__dirname, "node_modules/react-native-ratings"),
+      path.resolve(__dirname, "src"),
+    ],
+    addWebpackAlias({
+      "react-native$": "react-native-web",
+      "react-native-linear-gradient": "react-native-web-linear-gradient",
+    })
+  )
+);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button, createTheme, ThemeProvider } from "@rneui/themed";
+
+const theme = createTheme({});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
+      
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <Button title="I'm a button from React Native Elements" />
+          </header>
+        </div>
+      </ThemeProvider>
+
+      <style type="text/css">{`
+        @font-face {
+          font-family: 'MaterialIcons';
+          src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+        }
+
+        @font-face {
+          font-family: 'FontAwesome';
+          src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
+        }
+      `}</style>
+    </>
   );
 }
 
