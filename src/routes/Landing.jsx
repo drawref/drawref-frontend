86c79dc3e54b10306d5802497f237c21ef393f2c
Daniel Oaks <daniel@danieloaks.net>
2024-01-10 12:10:58 +1000

    Change card colours to match mockup

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -16,23 +16,23 @@ function Landing() {
           </h1>
           <div className="flex w-[60rem] max-w-full px-4 mx-auto flex-wrap justify-center items-center my-8 gap-8 md:gap-12">
             <Link to="/" className="block w-[15rem] px-3 py-3 shadow-card border-slate-200 border-[5px] flex-shrink-0">
-              <div className="w-full h-[12.5rem] bg-slate-300 bg-cover rounded" style={{ backgroundImage: `url(${pose})` }}></div>
+              <div className="w-full h-[12.5rem] bg-zinc-300 bg-cover rounded" style={{ backgroundImage: `url(${pose})` }}></div>
               <h2 className="font-medium mt-2 -mb-1 text-2xl">Poses</h2>
             </Link>
             <Link to="/" className="block w-[15rem] px-3 py-3 shadow-card border-slate-200 border-[5px] flex-shrink-0">
-              <div className="w-full h-[12.5rem] bg-slate-300 bg-cover rounded" style={{ backgroundImage: `url(${face})` }}></div>
+              <div className="w-full h-[12.5rem] bg-zinc-300 bg-cover rounded" style={{ backgroundImage: `url(${face})` }}></div>
               <h2 className="font-medium mt-2 -mb-1 text-2xl">Faces</h2>
             </Link>
             <Link to="/" className="block w-[15rem] px-3 py-3 shadow-card border-slate-200 border-[5px] flex-shrink-0">
-              <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
+              <div className="w-full h-[12.5rem] bg-zinc-300 rounded"></div>
               <h2 className="font-medium mt-2 -mb-1 text-2xl">Animals</h2>
             </Link>
             <Link to="/" className="block w-[15rem] px-3 py-3 shadow-card border-slate-200 border-[5px] flex-shrink-0">
-              <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
+              <div className="w-full h-[12.5rem] bg-zinc-300 rounded"></div>
               <h2 className="font-medium mt-2 -mb-1 text-2xl">Hands</h2>
             </Link>
             <Link to="/" className="block w-[15rem] px-3 py-3 shadow-card border-slate-200 border-[5px] flex-shrink-0">
-              <div className="w-full h-[12.5rem] bg-slate-300 rounded"></div>
+              <div className="w-full h-[12.5rem] bg-zinc-300 rounded"></div>
               <h2 className="font-medium mt-2 -mb-1 text-2xl">Plants</h2>
             </Link>
           </div>
