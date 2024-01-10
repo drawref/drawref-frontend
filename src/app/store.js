2faa76080f639c64ac01c605dd5a2571fb4d89cf
Daniel Oaks <daniel@danieloaks.net>
2024-01-11 08:50:15 +1000

    Start creating store slices

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+import { api } from './apiSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 })
+
+// configure listeners using the provided defaults
+setupListeners(store.dispatch)
