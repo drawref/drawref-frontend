975b2f4b8e13a0176dc472f8e603c26a42b2ca67
Daniel Oaks <daniel@danieloaks.net>
2024-01-13 09:45:16 +1000

    Update the source code link

--- src/components/TheFooter.jsx
+++ src/components/TheFooter.jsx
@@ -5,7 +5,7 @@ function TheFooter() {
         <div className="h-20 bg-gradient-to-b from-transparent to-white sm:hidden"></div>
         <footer className="flex items-center justify-center gap-1 border-t-[5px] border-t-secondary-200 bg-primary-600 px-2 pb-1.5 pt-1 text-white">
           {"DrawRef · "}
-          <a href="https://github.com/DanielOaks" className="text-[#d2ebf4]">
+          <a href="https://github.com/DanielOaks/drawref-frontend" className="text-[#d2ebf4]">
             Source Code
           </a>
         </footer>
