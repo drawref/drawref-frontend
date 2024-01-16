8276be081d1a5498797596d43cad647edb192faa
Daniel Oaks <daniel@danieloaks.net>
2024-01-16 20:21:52 +1000

    Remember and restore logins

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 import { api } from "./apiSlice";
 import { sessionMetadataSlice } from "./sessionMetadataSlice";
-import { userProfileSlice } from "./userProfileSlice";
+import { userProfileSlice, login, logout } from "./userProfileSlice";
+
+// listener middleware, used to send store data to local storage
+const listenerMiddleware = createListenerMiddleware();
+listenerMiddleware.startListening({
+  actionCreator: login,
+  effect: async (action, listenerApi) => {
+    localStorage.setItem("drawref-login", JSON.stringify(action.payload));
+  },
+});
+listenerMiddleware.startListening({
+  actionCreator: logout,
+  effect: async (action, listenerApi) => {
+    localStorage.removeItem("drawref-login");
+  },
+});
 
 export const store = configureStore({
   reducer: {
@@ -14,8 +29,16 @@ export const store = configureStore({
 
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware).concat(api.middleware),
 });
 
 // configure listeners using the provided defaults
 setupListeners(store.dispatch);
+
+// load details from local storage
+const loginDetails = JSON.parse(localStorage.getItem("drawref-login") || "{}");
+const loginExpirationTime = new Date(loginDetails.exp || "1995-12-17T03:24:00").getTime();
+if (loginDetails && new Date().getTime() < loginExpirationTime) {
+  store.dispatch(login(loginDetails));
+}
