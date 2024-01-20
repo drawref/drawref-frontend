3e8db2c7eea42985e04696ce83762e3d29794771
Daniel Oaks <daniel@danieloaks.net>
2024-01-20 18:24:09 +1000

    Improve cover image display

--- src/components/LandingCategoryCard.jsx
+++ src/components/LandingCategoryCard.jsx
@@ -8,7 +8,7 @@ function LandingCategoryCard({ categoryKey, name, imageUrl }) {
       className="block w-[15rem] flex-shrink-0 border-[5px] border-slate-200 px-3 py-3 shadow-card"
     >
       <div
-        className="h-[12.5rem] w-full rounded bg-zinc-300 bg-cover"
+        className="h-[12.5rem] w-full rounded bg-zinc-300 bg-cover bg-center"
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
       <h2 className="-mb-1 mt-2 text-2xl font-medium">{name}</h2>
