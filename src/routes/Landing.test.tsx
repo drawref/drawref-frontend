25e072e61e87d42c5cb0941048d1fe75168df9c7
Daniel Oaks <daniel@danieloaks.net>
2026-09-20 20:08:13 +1000

    Fix tests

--- src/routes/Landing.test.tsx
+++ src/routes/Landing.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Landing from "./Landing";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders page header", () => {
-  render(<Landing />);
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Landing />
+      </BrowserRouter>
+    </Provider>,
+  );
   const h1Element = screen.getByText(/select a category/i);
   expect(h1Element).toBeInTheDocument();
 });
--- src/routes/NotFound.test.tsx
+++ src/routes/NotFound.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import NotFound from "./NotFound";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders page content", () => {
-  render(<NotFound />);
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>
+    </Provider>,
+  );
   const h1Element = screen.getByText(/404 not found/i);
   expect(h1Element).toBeInTheDocument();
 });
